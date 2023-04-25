aea073c1d733734f3db098b7ee906fa6aedffddb
nicholascao <me@nicholasc.cn>
2023-04-25 18:05:36 +0800

    Style: delete useless

--- run2.sh
+++
@@ -1,1 +0,0 @@
-nohup bash scrtips/train_sft.sh > logs/bloom-1b7-sft.log 2>&1 &
--- scrtips/test.sh
+++
@@ -1,8 +0,0 @@
-python inference.py \
-    --model bloom \
-    --model_path outputs/bloom-1b7-instruction-tuning \
-    --input 'Hello' \
-    --temperature 0.8 \
-    --top_k 30 \
-    --max_length 128 \
-    --num_return_sequences 3
